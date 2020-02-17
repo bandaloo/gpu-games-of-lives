3d007039dd177a72e1fa51a3ef44912bea3b5a69
bandaloo <bandaloo@icloud.com>
2020-02-17 13:45:07 -0500

    corrected formatting and added additional comments

--- simulation.glsl
+++ simulation.glsl
@@ -14,9 +14,7 @@ vec4 getPixel(int x, int y) {
 
 // look up individual cell values
 int get(int x, int y) {
-  return int(
-    getPixel(x, y).r
-  );
+  return int(getPixel(x, y).r);
 }
 
 void main() {
@@ -33,16 +31,18 @@ void main() {
   vec4 color = getPixel(0, 0);
   vec4 newColor;
   
+  // red channel: alive (1.0) or dead (0.0)
+  // green channel: how recently cell died
+  // blue channel: age of alive cell
   if (sum == 3) {
-    // ideal # of neighbors... if cell is living, stay alive, if it is dead, come to life!
-    //gl_FragColor = vec4( 1. );
+    // ideal # of neighbors...
+    // if cell is living, stay alive, if it is dead, come to life!
     newColor = vec4(1.0, 0.0, 0.01 + color.b, 1.0);
-  }else if (sum == 2) {
+  } else if (sum == 2) {
     // maintain current state
     float current = float(get(0, 0));
-    //gl_FragColor = vec4( vec3( current ), 1.0 );
     newColor = vec4(color.r, color.g * 0.95, color.r * (0.01 + color.b), 1.0);
-  }else {
+  } else {
     // over-population or loneliness... cell dies
     newColor = vec4(0.0, color.r * 1.0 + color.g * 0.95, 0.0, 1.0);
   }
