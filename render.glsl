6e220f3fd020d542edc5c4b1bbc00d14b3d10b34
bandaloo <bandaloo@icloud.com>
2020-03-01 22:33:55 -0500

    added configurable dead color

--- render.glsl
+++ render.glsl
@@ -10,9 +10,11 @@ uniform vec2 resolution;
 vec4 youngColor = vec4(1.0, 1.0, 0.0, 1.0);
 vec4 oldColor = vec4(0.0, 1.0, 1.0, 1.0);
 vec4 trailColor = vec4(1.0, 0.0, 1.0, 1.0);
+vec4 deadColor = vec4(1.0, 1.0, 1.0, 1.0);
 
 void main() {
   vec4 originalColor = vec4(texture2D(uSampler, gl_FragCoord.xy / resolution).rgb, 1.0);
-  vec4 newColor = mix(youngColor, oldColor, originalColor.b) * originalColor.r + trailColor * originalColor.g;
+  vec4 newColor = mix(youngColor, oldColor, originalColor.b) * originalColor.r
+                  + mix(deadColor, trailColor, originalColor.g) * (1.0 - originalColor.r);
   gl_FragColor = newColor;
 }
